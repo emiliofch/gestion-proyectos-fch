0f2aecc7c3486c042c63fca0f9dc7547e92391a8
emiliofch <emilio.lopez@fch.cl>
2026-02-03 18:15:09 +0000

    Fix: Cambiar react-hot-toast por react-toastify

--- src/components/VistaOportunidades.jsx
+++ src/components/VistaOportunidades.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../supabaseClient'
 import * as XLSX from 'xlsx'
-import { toast } from 'react-hot-toast'
+import { toast } from 'react-toastify'
 
 export default function VistaOportunidades({ user }) {
   const [oportunidades, setOportunidades] = useState([])
